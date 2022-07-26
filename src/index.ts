28107a6d72db9738b47e756f0baf199c3123f599
Maciej Krawczyk <git@kravchyk.com>
2022-07-26 16:03:37 +0200

    feat: Expose getClientX and getClientY in package exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Draggable } from './draggable';
+
+import { getClientX, getClientY } from './util/dom';
+
 import type { Options, DraggableEvent } from './types';
 
-export { Draggable };
+export { Draggable, getClientX, getClientY };
 
 export type { Options, DraggableEvent };
