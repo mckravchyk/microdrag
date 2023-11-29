05f083bd76a3208a96e5f694562e3f1fb027ac0d
Maciej Krawczyk <git@kravchyk.com>
2023-11-29 19:31:49 +0100

    chore: Add package.json `exports` field

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -11,6 +11,7 @@ import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -68,7 +69,7 @@ const bundle = () => [
     input: 'src/index.ts',
     external,
     output: {
-      file: pkg.module,
+      file: pkg.exports['.'].import.default,
       format: 'es',
       banner,
     },
@@ -91,12 +92,22 @@ const bundle = () => [
     input: 'src/index.ts',
     external,
     output: {
-      file: pkg.main,
+      file: pkg.exports['.'].require.default,
       format: 'cjs',
       banner,
     },
     plugins: [
       typescript(),
+      copy({
+        targets: [
+          // TypeScript requires 2 distinct files for ESM and CJS types. See:
+          // https://devblogs.microsoft.com/typescript/announcing-typescript-4-7/
+          // https://github.com/gxmari007/vite-plugin-eslint/pull/60
+          // Copy for ESM types is made in CJS bundle to ensure the declaration file generated in
+          // the previous bundle exists.
+          { src: 'dist/types/index.d.ts', dest: 'dist/types', rename: 'index.d.mts' },
+        ],
+      }),
     ],
   },
 
